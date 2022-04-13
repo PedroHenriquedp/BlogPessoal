a2a6cec1a17c1a694cfb61be8c6f3837f4e91aeb
PedroHenriquedp <pereira.henrique@aluno.ufabc.edu.br>
2022-04-13 20:12:15 -0300

    Corrigindo o logar

--- src/main/java/org/generation/blogpessoal/security/UserDetailsImpl.java
+++ src/main/java/org/generation/blogpessoal/security/UserDetailsImpl.java
@@ -7,50 +7,59 @@ import org.generation.blogpessoal.model.Usuario;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public class UserDetailsImpl implements UserDetails{
+
+public class UserDetailsImpl implements UserDetails {
 	private static final long serialVersionUID = 1L;
 	
 	private String userName;
 	private String password;
 	private List<GrantedAuthority> authorities;
 	
+	
 	public UserDetailsImpl(Usuario usuario) {
 		
 		userName = usuario.getUsuario();
-		password = usuario.getSenha();	
+		password = usuario.getSenha();
 	}
 	
-	public UserDetailsImpl() {
-		
-	}
-
+	public UserDetailsImpl() {}
+	
+	
 	@Override
 	public String getPassword() {
 		return password;
 	}
+	
 	@Override
 	public String getUsername() {
 		return userName;
 	}
+	
+	
+	//metodos padrão basic security
 	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities(){
+	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return authorities;
 	}
-	//métodos padrão basic security
+	
 	@Override
 	public boolean isAccountNonExpired() {
 		return true;
 	}
+	
 	@Override
 	public boolean isAccountNonLocked() {
 		return true;
 	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
 	@Override
 	public boolean isEnabled() {
 		return true;
 	}
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return false;
-	}
+	
 }
